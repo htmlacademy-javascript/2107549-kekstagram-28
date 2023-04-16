d51476d511fffa96818d8959be2ed7061db3ab61
Nikolay <h09boi@gmail.com>
2023-04-16 18:14:59 +0300

    fix filter previews

--- js/load-picture.js
+++ js/load-picture.js
@@ -1,14 +1,19 @@
 const FILE_TYPES = ['jpg', 'jpeg', 'png'];
 
 const fileChooser = document.querySelector('.img-upload__start input[type=file]');
-const preview = document.querySelector('.img-upload__preview img');
+const previewElement = document.querySelector('.img-upload__preview img');
+const previewMiniatureNodeList = document.querySelectorAll('.effects__preview');
 
 fileChooser.addEventListener('change', () => {
   const file = fileChooser.files[0];
   const fileName = file.name.toLowerCase();
-  const matches = FILE_TYPES.some((it) => fileName.endsWith(it));
+  const isCorrectFileType = FILE_TYPES.some((it) => fileName.endsWith(it));
 
-  if (matches) {
-    preview.src = URL.createObjectURL(file);
+  if (isCorrectFileType) {
+    const imageFromUrl = URL.createObjectURL(file);
+    previewMiniatureNodeList.forEach((el) => {
+      el.style.backgroundImage = `url(${imageFromUrl})`;
+    });
+    previewElement.src = imageFromUrl;
   }
 });
